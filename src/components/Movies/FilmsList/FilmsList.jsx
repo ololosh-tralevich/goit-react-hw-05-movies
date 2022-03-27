5c3f2d67467c22aaf1f4dfa5a70eeda1a2f0988a
ololosh-tralevich <karpeko76@gmail.com>
2022-03-28 00:16:46 +0100

    upd

--- src/components/Movies/FilmsList/FilmsList.jsx
+++ src/components/Movies/FilmsList/FilmsList.jsx
@@ -5,14 +5,28 @@ import style from './filmsList.module.css';
 const FilmsList = ({ films, filmIdFunc }) => {
   const partOfCode = films.map(film => {
     return (
-      <li className={style.filmListItem} key={film.id} onClick={() => filmIdFunc(film.id)}>
+      <li
+        className={style.filmListItem}
+        key={film.id}
+        onClick={() => filmIdFunc(film.id)}
+      >
         <Link to={`/movies/${film.id}`}>
-          <img
-            className={style.filmListItemImg}
-            src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
-            alt="Film Poster"
-            loading="lazy"
-          ></img>
+          {film.poster_path ? (
+            <img
+              className={style.filmListItemImg}
+              src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+              alt="Film Poster"
+              loading="lazy"
+            ></img>
+          ) : (
+            <img
+              className={style.filmListItemImg}
+              src="https://media.istockphoto.com/vectors/no-image-available-icon-vector-id1216251206?k=20&m=1216251206&s=170667a&w=0&h=A72dFkHkDdSfmT6iWl6eMN9t_JZmqGeMoAycP-LMAw4="
+              alt="Film Poster"
+              loading="lazy"
+            ></img>
+          )}
+
           {film.title ? (
             <h4 className={style.filmTitle}>{film.title}</h4>
           ) : (
--- src/components/Movies/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/Movies/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,12 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { getFullMovieInfo } from '../fetchFilms/fetchFilms';
 
 import style from './movieDetailsPage.module.css';
 
 const MovieDetailsPage = ({ filmId }) => {
+//   const firstRender = useRef(true);
   const [film, setFilm] = useState({});
   const [filmGenres, setFilmGenres] = useState('');
+
+  //   useEffect(() => {
+  //     if (firstRender.current) {
+  //       takeFetchData();
+  //       firstRender.current = false;
+  //     }
+  //   }, []);
 
   useEffect(() => {
     takeFetchData();
@@ -35,14 +43,26 @@ const MovieDetailsPage = ({ filmId }) => {
 
   return (
     <div className={style.mainBlock}>
-      <Link to="/movies" className={style.goBackLink}>Go Back</Link>
+      <Link to="/movies" className={style.goBackLink}>
+        Go Back
+      </Link>
       <div className={style.filmInfoMain}>
         <div className={style.filmPoster}>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
-            className={style.filmImg}
-            alt="Movie Poster"
-          />
+          {film.poster_path ? (
+            <img
+              className={style.filmImg}
+              src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+              alt="Film Poster"
+              loading="lazy"
+            ></img>
+          ) : (
+            <img
+              className={style.filmImg}
+              src="https://media.istockphoto.com/vectors/no-image-available-icon-vector-id1216251206?k=20&m=1216251206&s=170667a&w=0&h=A72dFkHkDdSfmT6iWl6eMN9t_JZmqGeMoAycP-LMAw4="
+              alt="Film Poster"
+              loading="lazy"
+            ></img>
+          )}
         </div>
         <div className={style.filmInfo}>
           {film.title ? (
@@ -58,7 +78,7 @@ const MovieDetailsPage = ({ filmId }) => {
         </div>
       </div>
       <div className={style.additionalInfo}>
-          <h3>Additional Info:</h3>
+        <h3>Additional Info:</h3>
       </div>
     </div>
   );
