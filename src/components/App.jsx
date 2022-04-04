2c0e6c224ec81f2973d6e4994ce29089cc66a9dd
ololosh-tralevich <karpeko76@gmail.com>
2022-04-04 20:46:12 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import LayoutPage from '../pages/LayoutPage/LayoutPage';
